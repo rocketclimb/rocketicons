250575b4398d9545930d22afa4221859e1815c9c
Jeferson Amorim <amorimjj@gmail.com>
2024-03-23 23:27:57 -0300

    Add web package

---
+++ packages/ignition/src/app/page.tsx
@@ -0,0 +1,17 @@
+import * as Icon from "@rocket-climb/bolt/fa";
+import Image from "next/image";
+
+// const [A] = Object.values(Icon);
+// console.log(A);
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-wrap">
+        {Object.values(Icon).map((Icon, i) => (
+          <Icon key={i} className="fill-sky-800" />
+        ))}
+      </div>
+    </main>
+  );
+}
