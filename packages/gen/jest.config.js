a552165ab6ddec3057e9d89ce1e1021106759009
Jeferson Amorim <amorimjj@gmail.com>
2024-03-25 08:42:33 -0300

    Fix package scripts

---
+++ packages/gen/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
+  modulePaths: [compilerOptions.baseUrl],
+};
