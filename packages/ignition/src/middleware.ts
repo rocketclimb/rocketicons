5eefc98266b8f2703187ea489cd6130cd84e9b4c
Jeferson Amorim <amorimjj@gmail.com>
2024-03-27 12:10:32 -0300

    Handle locales

---
+++ packages/ignition/src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const locales = ["en", "pt-br"];
+const defaultLocale = "en";
+
+const getLocale = (request: NextRequest): string =>
+  (
+    request.headers
+      .get("accept-language")
+      ?.split(",")
+      .map((language) => language.split(";").shift())
+      .find(
+        (language) => language && locales.includes(language.toLocaleLowerCase())
+      ) || defaultLocale
+  ).toLowerCase();
+
+export const middleware = (request: NextRequest) => {
+  const locale = getLocale(request);
+  const { pathname } = request.nextUrl;
+
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) {
+    return;
+  }
+
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  const isDefaultLocale = locale === defaultLocale;
+
+  return isDefaultLocale
+    ? NextResponse.rewrite(request.nextUrl)
+    : NextResponse.redirect(request.nextUrl);
+};
+
+export const config = {
+  matcher: ["/((?!_next|img).*)"],
+};
