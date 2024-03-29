2497f93eb0366c54b575c897c9e7ef899b4fb439
Jeferson Amorim <amorimjj@gmail.com>
2024-03-28 21:58:05 -0300

    Fix class name removing false from generated classes

--- packages/core/src/utils/handle-class-name.ts
+++ packages/core/src/utils/handle-class-name.ts
@@ -1,6 +1,8 @@
 import { Variants } from "@/types";
 
 export const handleClassName = (variant: Variants, className: string): string =>
-  `${variant === "filled" && "icon-filled"} ${
-    variant === "outlined" && "icon-outlined"
+  `${
+    (variant === "filled" && "icon-filled") ||
+    (variant === "outlined" && "icon-outlined") ||
+    ""
   } ${(!className?.match(/icon\-/) && `icon-default`) || ""} ${className}`;
