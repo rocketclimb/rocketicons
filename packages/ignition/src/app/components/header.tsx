07a0d7e582444eccbc01eafa34f8e774e241226b
Jeferson Amorim <amorimjj@gmail.com>
2024-03-31 19:34:02 -0300

    Fix minor layout issue

--- packages/ignition/src/app/components/header.tsx
+++ packages/ignition/src/app/components/header.tsx
@@ -15,7 +15,7 @@ const LogoLink = () => {
 
 const Header = ({ lang }: PropsWithLang) => {
   return (
-    <div className="sticky flex top-0 z-40 w-full h-16 px-8 py-4 items-center backdrop-blur flex-none transition-colors duration-500 bg-white/95 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] dark:bg-transparent">
+    <div className="sticky flex top-0 z-40 w-full h-16 px-8 py-4 items-center backdrop-blur flex-none transition-colors duration-500 bg-white/95 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] dark:bg-transparent xl:mx-auto max-w-screen-2xl">
       <LogoLink />
       <MainNav lang={lang} />
     </div>
