a4b8259feb4564cc8d2b468bb4085ada72574975
Daniel Gomes <programad@gmail.com>
2024-04-01 10:25:44 -0300

    removing console.log

--- packages/ignition/src/app/utils/map-utils.ts
+++ packages/ignition/src/app/utils/map-utils.ts
@@ -5,8 +5,6 @@ interface Array<T> {
 Array.prototype.groupBy = function <K, V>(
   grouper: (item: V) => K
 ): Map<K, Array<V>> {
-  console.log("groupBy called");
-
   return this.reduce((store: Map<K, Array<V>>, item: V) => {
     const key = grouper(item);
     if (!store.has(key)) {
