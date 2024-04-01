394317bd3c478ee3d54e6bccee7fb7b1a09da7cd
Daniel Gomes <programad@gmail.com>
2024-04-01 15:43:33 -0300

    minor navigation fixes

--- packages/ignition/src/app/[lang]/(content)/docs/[slug]/page.tsx
+++ packages/ignition/src/app/[lang]/(content)/docs/[slug]/page.tsx
@@ -1,5 +1,20 @@
 import { MdxDoc } from "@/components/mdx";
+import { Metadata } from "next";
 import { PropsWithLangSlugParams } from "@/app/types/props-with-lang-and-slug-param";
+import { useLocale } from "@/app/locales";
+
+export const generateMetadata = ({
+  params: { lang, slug },
+}: PropsWithLangSlugParams): Metadata => {
+  const { title, description } = useLocale(lang, slug).component() as {
+    title: string;
+    description: string;
+  };
+  return {
+    title: `${title} | rocketicons`,
+    description,
+  };
+};
 
 const Page = ({ params: { lang, slug } }: PropsWithLangSlugParams) => {
   return (
