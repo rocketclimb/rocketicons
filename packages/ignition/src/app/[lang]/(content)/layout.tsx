b62af61ab4089f43d953420def8eb8300d75049f
Jeferson Amorim <amorimjj@gmail.com>
2024-04-01 12:28:06 -0300

    Style scrollbar

--- packages/ignition/src/app/[lang]/(content)/layout.tsx
+++ packages/ignition/src/app/[lang]/(content)/layout.tsx
@@ -6,11 +6,11 @@ const Layout = ({
   params: { lang },
 }: PropsWithChildrenAndLangParams) => (
   <>
-    <div className="group content-area pb-10 lg:pr-7 lg:overflow-y-auto">
+    <div className="group content-area pb-10 lg:pr-7 lg:hover:overflow-y-auto">
       <CollapsedSidebar lang={lang} />
       <SidebarLeft lang={lang} />
     </div>
-    <div className="grow content-area pb-10 pr-7 overflow-y-auto">
+    <div className="grow content-area pb-10 pr-7 hover:overflow-y-auto">
       <article className="pt-5">{children}</article>
       <div className="relative mx-auto w-full max-w-screen-xl px-4 py-10 md:flex md:flex-row md:py-10">
         footer
