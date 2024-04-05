325b8412487d5291b689a0ce3c6467684087f96c
Jeferson Amorim <amorimjj@gmail.com>
2024-04-05 07:29:13 -0300

    Use rocketclimb text component

--- packages/ignition/src/app/[lang]/(content)/footer.tsx
+++ packages/ignition/src/app/[lang]/(content)/footer.tsx
@@ -1,12 +1,15 @@
 import { IoLogoGithub } from "rocketicons/io";
 import React from "react";
+import RocketClimbText from "@/app/components/rocketclimb-text";
 
 const Footer = () => {
   return (
     <footer className="w-full mt-8">
       <div className="px-4 pt-4 pb-5 border-t border-slate-200 flex justify-between text-slate-500 dark:border-slate-200/5">
         <div>
-          <p>Copyright © 2024 RocketClimb.</p>
+          <p>
+            Copyright © 2024 <RocketClimbText />
+          </p>
         </div>
         <div className="leading-5 text-slate-400 dark:text-slate-500">
           <a
