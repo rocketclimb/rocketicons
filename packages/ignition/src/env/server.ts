664e19dfba5ca8f3fc6ad862c8d4b72791f20179
Daniel Gomes <programad@gmail.com>
2024-04-08 12:19:52 -0300

    fixing env variable

--- packages/ignition/src/env/server.ts
+++ packages/ignition/src/env/server.ts
@@ -5,5 +5,5 @@ type serverSchema = {
 
 export const serverEnv = {
   NEXT_PUBLIC_APP_URL: String(process.env.NEXT_PUBLIC_APP_URL),
-  GOOGLE_ANALYTICS_ID: String(process.env.GOOGLE_ANALYTICS_ID),
+  GOOGLE_ANALYTICS_ID: String(process.env.NEXT_GOOGLE_ANALYTICS_ID),
 } satisfies serverSchema;
