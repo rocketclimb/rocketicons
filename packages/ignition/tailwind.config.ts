9c3bb4549e58672db7595a53d4658ca2db6c55ae
Daniel Gomes <programad@gmail.com>
2024-04-10 20:50:15 -0300

    fix: tailwind cleanup

--- packages/ignition/tailwind.config.ts
+++ packages/ignition/tailwind.config.ts
@@ -72,35 +72,6 @@ const config: Config = {
     {
       pattern: /icon-*/,
     },
-    "bg-black",
-    "list-disc",
-    "list-decimal",
-    "list-inside",
   ],
-  components: {
-    icon: {
-      default: "sky-500-base",
-      baseStyle: "p-0 inline-block",
-      variants: {
-        filled: "",
-        outlined: "",
-      },
-      sizes: {
-        xs: "size-2",
-        sm: "size-4",
-        base: "size-5",
-        lg: "size-6",
-        xl: "size-7",
-        "2xl": "size-8",
-        "3xl": "size-9",
-        "4xl": "size-10",
-        "5xl": "size-11",
-        "6xl": "size-12",
-        "7xl": "size-14",
-      },
-    },
-  },
 };
 export default config;
-
-// color não extende em icons, só em theme (tailwind)
