e38b81581a54d89e89e27b4d6874786369666438
Jeferson Amorim <amorimjj@gmail.com>
2024-04-14 10:19:52 -0300

    Merging

--- packages/ignition/next.config.mjs
+++ packages/ignition/next.config.mjs
@@ -29,5 +29,4 @@ const nextConfig = {
   },
 };
 
-// export default withContentCollections(withMDX(nextConfig));
-export default withMDX(nextConfig);
+export default withContentCollections(withMDX(nextConfig));
