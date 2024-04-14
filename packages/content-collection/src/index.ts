667ee6ab59778460abb13a4de1dac91cc465b791
Daniel Gomes <programad@gmail.com>
2024-04-14 11:49:01 -0300

    feat: content-collections moved out of here

--- packages/content-collection/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { withContentCollections, createContentCollectionPlugin } from "./next";
-
-export * from "./config";
-export * from "./builder";
-export type { GetTypeByName, Modification } from "./types";
-
-export { TransformError } from "./transformer";
-export { CollectError } from "./collector";
-export { ConfigurationError } from "./configurationReader";
