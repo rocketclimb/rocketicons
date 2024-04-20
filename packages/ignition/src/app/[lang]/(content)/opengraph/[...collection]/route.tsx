00e28e70aaa7efc9caa453d1600e439ef1477a88
Daniel Gomes <programad@gmail.com>
2024-04-20 18:14:30 -0300

    fixing vercel build

--- packages/ignition/src/app/[lang]/(content)/opengraph/[...collection]/route.tsx
+++ packages/ignition/src/app/[lang]/(content)/opengraph/[...collection]/route.tsx
@@ -6,14 +6,6 @@ import { NextRequest } from "next/server";
 import * as changeCase from "change-case";
 import { IconsManifest } from "rocketicons/data";
 
-export const runtime = "edge";
-export const alt = "rocketicons - React Icons like you haver seen before!";
-export const size = {
-  width: 1200,
-  height: 630,
-};
-export const contentType = "image/png";
-
 export const GET = async (request: NextRequest) => {
   const [, lang, , collectionId, iconId] = request.nextUrl.pathname.split("/");
   const iconName = !!iconId ? changeCase.pascalCase(iconId) : undefined;
