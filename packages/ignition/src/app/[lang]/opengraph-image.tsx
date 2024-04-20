e1843d75390c507cc063fe859c45f165f2e8bf02
Daniel Gomes <programad@gmail.com>
2024-04-20 19:07:15 -0300

    build fix

--- packages/ignition/src/app/[lang]/opengraph-image.tsx
+++ packages/ignition/src/app/[lang]/opengraph-image.tsx
@@ -1,9 +1,5 @@
 import OpenGraph from "@/components/opengraph";
 import { PropsWithLangParams } from "@/types";
-import dynamic from "next/dynamic";
-import { RcRocketIcon } from "rocketicons/rc";
-import OgRc from "./opengraph-image-rc";
-import OgFa from "./opengraph-image-fa";
 
 export const runtime = "edge";
 export const alt = "rocketicons - React Icons like you haver seen before!";
