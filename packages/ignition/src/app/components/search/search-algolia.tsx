5e4c2cf3409c2922003c79d99593492e4e4e5836
Jeferson Amorim <amorimjj@gmail.com>
2024-04-22 07:43:50 -0300

    Update search to match layout

--- packages/ignition/src/app/components/search/search-algolia.tsx
+++ packages/ignition/src/app/components/search/search-algolia.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { InstantSearch } from "react-instantsearch-dom";
 import algoliasearch from "algoliasearch/lite";
 import SearchHits from "@/components/search/search-hits";
@@ -5,27 +6,19 @@ import { siteConfig } from "@/config/site";
 import { serverEnv } from "@/env/server";
 import { useLocale } from "@/locales";
 import { Languages } from "@/types";
-import SearchBoxInnerContentsAlgolia from "./search-box-inner-contents-algolia";
+import SearchBox from "./search-box";
 
 type SearchButtonProps = {
   lang: Languages;
-  inputRef?: React.RefObject<HTMLInputElement>;
+  close: () => void;
 };
 
-const SearchAlgolia = ({ lang, inputRef }: SearchButtonProps) => {
-  const search = useLocale(lang).config("search");
+const SearchAlgolia = ({ lang, close }: SearchButtonProps) => {
+  const { placeholder } = useLocale(lang).config("search");
 
   const searchClient = algoliasearch(
     serverEnv.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
     serverEnv.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY
-  );
-
-  const SearchBoxInnerComponent = ({ className }: { className?: string }) => (
-    <SearchBoxInnerContentsAlgolia
-      label={search["placeholder"]}
-      inputRef={inputRef}
-      className={className}
-    />
   );
 
   return (
@@ -34,13 +27,21 @@ const SearchAlgolia = ({ lang, inputRef }: SearchButtonProps) => {
         searchClient={searchClient}
         indexName={`${siteConfig.name}-${lang}`}
       >
-        <div className="fixed w-full h-full inset-0 flex flex-col items-center justify-center pointer-events-none">
-          <div className="w-1/3 pointer-events-auto">
-            <SearchBoxInnerComponent />
-          </div>
-
-          <div className="w-1/2 pointer-events-auto">
-            <SearchHits lang={lang} />
+        <div className="fixed w-full h-full inset-0 flex flex-col items-center pt-8 lg:pt-[10vh] px-2 pointer-events-none">
+          <div className="w-full max-w-2xl pointer-events-auto rounded-lg text-slate-900 dark:text-slate-200 bg-white dark:bg-slate-800">
+            <div className="w-full mb-2">
+              <header className="flex items-center w-full px-4 border-b border-slate-100 dark:border-slate-200/5">
+                <SearchBox label={placeholder} />
+                <button type="reset" aria-label="Cancel" onClick={close}>
+                  <abbr className="text-slate-900 dark:text-slate-400 border dark:border-0 border-slate-200 dark:bg-slate-600 p-1.5 rounded-md text-[8px] font-sans font-semibold">
+                    ESC
+                  </abbr>
+                </button>
+              </header>
+            </div>
+            <div className="w-full pointer-events-auto">
+              <SearchHits lang={lang} />
+            </div>
           </div>
         </div>
       </InstantSearch>
