3031daebe377c539697896e69fd5dd432b9f2219
Jeferson Amorim <amorimjj@gmail.com>
2024-04-22 00:11:54 -0300

    Refactory with copy to share across application

--- packages/ignition/src/app/components/code-block/with-copy.tsx
+++ packages/ignition/src/app/components/code-block/with-copy.tsx
@@ -1,30 +1,21 @@
 "use client";
-import useClipboard from "@/hooks/use-clipboard";
-import { useLocale } from "@/locales";
+import BaseWithCopy from "@/components/documentation/with-copy";
 import { PropsWithChildrenAndLang } from "@/types";
 
 type WithCopyProps = {
   clipboardText: string;
 } & PropsWithChildrenAndLang;
 
-const WithCopy = ({ lang, clipboardText, children }: WithCopyProps) => {
-  const { copy, copied } = useLocale(lang).config("code-block");
-
-  const { write, isCurrent } = useClipboard(clipboardText);
-  return (
-    <button
-      data-copied={isCurrent()}
-      data-txt-copy={copy}
-      data-txt-copied={`${copied} ✓`}
-      onClick={() => write()}
-      onKeyDown={({ key }) => key === "c" && write()}
-      className="group/code-block text-left inline-block w-full min-w-32 after:hidden after:text-xs after:right-5 after:top-0 after:absolute after:w-30 text-slate-50 dark:text-slate-400 after:border-slate-700 after:rounded after:py-0.5 after:px-1 hover:after:block hover:after:bg-slate-900 hover:after:border hover:after:content-[attr(data-txt-copy)] data-[copied=true]:after:content-[attr(data-txt-copied)]"
-    >
-      <span className="group-hover/code-block:opacity-60 w-full inline-block">
-        {children}
-      </span>
-    </button>
-  );
-};
+const WithCopy = ({ lang, clipboardText, children }: WithCopyProps) => (
+  <BaseWithCopy
+    lang={lang}
+    clipboardText={clipboardText}
+    className="group/code-block text-left inline-block w-full min-w-32 text-slate-50 dark:text-slate-400 after:text-xs after:-right-2 after:-top-1.5"
+  >
+    <span className="group-hover/code-block:opacity-60 w-full inline-block">
+      {children}
+    </span>
+  </BaseWithCopy>
+);
 
 export default WithCopy;
