1fd7662e3dcbcc4e9b19341b10a2fe3aac402d35
Jeferson Amorim <amorimjj@gmail.com>
2024-04-22 21:02:35 -0300

    Fix #49 grouping searching results

--- packages/ignition/src/app/components/search/search-hits.tsx
+++ packages/ignition/src/app/components/search/search-hits.tsx
@@ -1,7 +1,7 @@
 import { connectStateResults, Highlight } from "react-instantsearch-dom";
 import Link from "next/link";
 import { useLocale } from "@/locales";
-import { Languages, PropsWithLang } from "@/types";
+import { PropsWithLang } from "@/types";
 import { SiAlgolia } from "rocketicons/si";
 import IconLoader from "@/components/icons/icon-loader";
 import { BiLoaderAlt } from "rocketicons/bi";
@@ -42,7 +42,11 @@ const IconHit = ({ hit, lang }: IconHitProps) => (
   </>
 );
 
-const Hit = (hit: any, lang: Languages) => {
+type PropsHit = {
+  hit: any;
+} & PropsWithLang;
+
+const Hit = ({ hit: { __highlightResult, ...hit }, lang }: PropsHit) => {
   const groupSlug = useLocale(hit.locale || lang).doc(hit.group)?.slug;
 
   return (
@@ -54,7 +58,12 @@ const Hit = (hit: any, lang: Languages) => {
           : `/${hit.locale}/docs/${hit.objectID}`
       }
     >
-      <Highlight attribute="title" hit={hit} tagName="mark" />
+      <Highlight
+        attribute="title"
+        highlightPropert={__highlightResult}
+        hit={hit}
+        tagName="mark"
+      />
     </Link>
   );
 };
@@ -81,36 +90,80 @@ const Importer = ({ component, lang }: ImporterProps) => (
   </WithCopy>
 );
 
+type ResultTitleProps = {
+  label: string;
+} & PropsWithChildren;
+
+const ResultTitle = ({ label, children }: ResultTitleProps) => (
+  <div className="text-slate-900 dark:text-slate-200 flex cursor-default pt-6 mb-4 leading-6">
+    <h2 className="grow font-semibold">{label}</h2>
+    {children}
+  </div>
+);
+
+const ResultItem = ({ children }: PropsWithChildren) => (
+  <li className="group/result w-full flex rounded-lg mb-2 text-sm font-normal text-slate-500 dark:text-slate-200 hover:text-white bg-slate-50 dark:bg-slate-700/30 hover:bg-sky-500 hover:dark:bg-sky-500">
+    {children}
+  </li>
+);
+
+type HitResultProps = {
+  group: string;
+  hits: any;
+} & PropsWithLang;
+
+type IconResultProps = {
+  id: string;
+} & HitResultProps;
+
+const IconResult = ({ id, group, hits, lang }: IconResultProps) => (
+  <div className="m-2">
+    <ResultTitle label={group}>
+      <Importer lang={lang} component={`rocketicons/${id}`} />
+    </ResultTitle>
+    <ul>
+      {(hits as any).map((hit: any) => (
+        <ResultItem key={hit.objectID}>
+          <IconHit lang={lang} hit={hit} />
+        </ResultItem>
+      ))}
+    </ul>
+  </div>
+);
+
+const HitResult = ({ group, hits, lang }: HitResultProps) => (
+  <div className="m-2">
+    <ResultTitle label={group} />
+    <ul>
+      {(hits as any).map((hit: any) => (
+        <ResultItem key={hit.objectID}>
+          <Hit lang={lang} hit={hit} />
+        </ResultItem>
+      ))}
+    </ul>
+  </div>
+);
+
 type GroupedHitsProps = {
   groupedHits: any;
 } & PropsWithLang;
+
+const IconsGroupedHits = ({ lang, groupedHits }: GroupedHitsProps) =>
+  Object.values(groupedHits || {})
+    .sort(({ group: groupA }: any, { group: groupB }: any) =>
+      groupA.localeCompare(groupB)
+    )
+    .map(({ id, group, hits }: any) => (
+      <IconResult key={group} id={id} group={group} lang={lang} hits={hits} />
+    ));
 
 const GroupedHits = ({ lang, groupedHits }: GroupedHitsProps) =>
   Object.values(groupedHits || {})
     .sort(({ group: groupA }: any, { group: groupB }: any) =>
       groupA.localeCompare(groupB)
     )
-    .map(({ id, group, isIcon, hits }: any) => (
-      <div key={group} className="m-2">
-        <div className="text-slate-900 dark:text-slate-200 flex cursor-default pt-6 mb-4 leading-6">
-          <h2 className="grow font-semibold">{group}</h2>
-          {isIcon && <Importer lang={lang} component={`rocketicons/${id}`} />}
-        </div>
-        <ul>
-          {(hits as any).map((hit: any) => (
-            <li
-              key={hit.objectID}
-              className="group/result w-full flex rounded-lg mb-2 text-sm font-normal text-slate-500 dark:text-slate-200 hover:text-white bg-slate-50 dark:bg-slate-700/30 hover:bg-sky-500 hover:dark:bg-sky-500"
-            >
-              {hit.isIcon ? (
-                <IconHit lang={lang} hit={hit} />
-              ) : (
-                <Hit lang={lang} {...hit} />
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+    .map(({ group, hits }: any) => (
+      <HitResult key={group} group={group} lang={lang} hits={hits} />
     ));
 
 const SearchHits = ({
@@ -122,19 +175,23 @@ const SearchHits = ({
 } & PropsWithLang) => {
   const { "no-results": noResults } = useLocale(lang).config("search");
 
-  const groupedHits = searchResults?.hits.reduce((groups: any, hit: any) => {
-    if (hit.group) {
-      const key = hit.groupName || hit.group;
-      groups[key] = groups[key] || {
-        id: hit.group,
-        group: key,
-        isIcon: hit.isIcon,
-        hits: [],
-      };
-      groups[key].hits.push(hit);
-    }
-    return groups;
-  }, {});
+  const groupedHits = searchResults?.hits.reduce(
+    (groups: any, hit: any) => {
+      if (hit.group) {
+        const key = hit.groupName || hit.group;
+        const group = hit.isIcon ? groups["icons"] : groups["documents"];
+        group[key] = group[key] || {
+          id: hit.group,
+          group: key,
+          isIcon: hit.isIcon,
+          hits: [],
+        };
+        group[key].hits.push(hit);
+      }
+      return groups;
+    },
+    { icons: {}, documents: {} }
+  );
 
   return (
     <>
@@ -145,9 +202,14 @@ const SearchHits = ({
         <div
           className={`px-2 h-full min-h-40 max-h-[600px] lg:max-h-[65vh] overflow-auto thin-scroll ${borderClass}`}
         >
-          {searchResults?.hits.length > 0 && groupedHits && (
-            <GroupedHits groupedHits={groupedHits} lang={lang} />
-          )}
+          {searchResults?.hits.length > 0 &&
+            Object.keys(groupedHits.icons).length > 0 && (
+              <IconsGroupedHits groupedHits={groupedHits.icons} lang={lang} />
+            )}
+          {searchResults?.hits.length > 0 &&
+            Object.keys(groupedHits.documents).length > 0 && (
+              <GroupedHits groupedHits={groupedHits.documents} lang={lang} />
+            )}
         </div>
       </div>
       <div className={`p-4 text-right h-14 border-t ${borderClass}`}>
