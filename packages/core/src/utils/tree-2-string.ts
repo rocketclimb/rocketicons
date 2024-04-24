c84949dfa5d45d594f370426b338005ed43891c5
Jeferson Amorim <amorimjj@gmail.com>
2024-04-24 15:08:33 -0300

    Remove any from type

--- packages/core/src/utils/tree-2-string.ts
+++ packages/core/src/utils/tree-2-string.ts
@@ -1,11 +1,11 @@
 import { IconTree } from "@/types";
 
-export const attrToString = (attr: any): string =>
+export const attrToString = (attr: Record<string, string>): string =>
   Object.entries(attr)
     .map(([key, value]) => `${key}="${value}"`)
     .join("");
 
-const elementToString = (tag: string, attr: any, children: string): string =>
+const elementToString = (tag: string, attr:  Record<string, string>, children: string): string =>
   `<${tag} ${attrToString(attr)}>${children}</${tag}>`;
 
 export const tree2String = (tree: IconTree[]): string =>
