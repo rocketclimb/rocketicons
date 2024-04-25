5c2babc14d5008639a5bb8b8b335fd8654379da8
Jeferson Amorim <amorimjj@gmail.com>
2024-04-25 09:02:56 -0300

    Move sizing definition into a json

--- packages/ignition/src/app/components/usage/sizing/utils.ts
+++ packages/ignition/src/app/components/usage/sizing/utils.ts
@@ -1,85 +1,8 @@
+import definitions from "./sizes.json";
 export const sizesUtilities = ["size-5", "size-10", "size-14", "size-16"];
 
 export const hwUtilities = ["h-2 w-2", "h-4 w-4", "h-7 w-7", "h-9 w-9"];
 
 export const otherSizes = [...sizesUtilities, ...hwUtilities];
 
-export const sizes = {
-  xs: {
-    tw: "size-2",
-    props: [
-      { width: "0.5rem", comment: "8px" },
-      { height: "0.5rem", comment: "8px" }
-    ]
-  },
-  sm: {
-    tw: "size-4",
-    props: [
-      { width: "1rem;", comment: "16px" },
-      { height: "1rem", comment: "16px" }
-    ]
-  },
-  base: {
-    tw: "size-5",
-    props: [
-      { width: "1.25rem;", comment: "20px" },
-      { height: "1.25rem", comment: "20px" }
-    ]
-  },
-  lg: {
-    tw: "size-6",
-    props: [
-      { width: "1.5rem;", comment: "24px" },
-      { height: "1.5rem", comment: "24px" }
-    ]
-  },
-  xl: {
-    tw: "size-7",
-    props: [
-      { width: "1.75rem;", comment: "28px" },
-      { height: "1.75rem", comment: "28px" }
-    ]
-  },
-  "2xl": {
-    tw: "size-8",
-    props: [
-      { width: "2rem;", comment: "32px" },
-      { height: "2rem", comment: "32px" }
-    ]
-  },
-  "3xl": {
-    tw: "size-9",
-    props: [
-      { width: "2.25rem;", comment: "36px" },
-      { height: "2.25rem", comment: "36px" }
-    ]
-  },
-  "4xl": {
-    tw: "size-10",
-    props: [
-      { width: "2.5rem;", comment: "40px" },
-      { height: "2.5rem", comment: "40px" }
-    ]
-  },
-  "5xl": {
-    tw: "size-11",
-    props: [
-      { width: "2.75rem;", comment: "44px" },
-      { height: "2.75rem", comment: "44px" }
-    ]
-  },
-  "6xl": {
-    tw: "size-12",
-    props: [
-      { width: "3rem;", comment: "48px" },
-      { height: "3rem", comment: "48px" }
-    ]
-  },
-  "7xl": {
-    tw: "size-14",
-    props: [
-      { width: "3.5rem;", comment: "56px" },
-      { height: "3.5rem", comment: "56px" }
-    ]
-  }
-};
+export const sizes = definitions;
