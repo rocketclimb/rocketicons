7bfbae351f7e3849f989ee5ed19b9437112255f9
Daniel Gomes <programad@gmail.com>
2024-04-25 19:32:10 -0300

    fix(sonar): fixing some sonar Issues

--- packages/ignition/src/app/[lang]/(content)/roadmap/page.tsx
+++ packages/ignition/src/app/[lang]/(content)/roadmap/page.tsx
@@ -13,72 +13,78 @@ const Roadmap = async ({ params: { lang } }: PropsWithLangParams) => {
     await fs.readFile(process.cwd() + `/src/app/locales/roadmap.json`, "utf-8")
   ) as RoadmapFile;
 
-  const itemList = roadmapFile?.items.map((item: any, i: number) => (
-    <li key={`roadmap-item-${i}`}>
-      <div className={`relative ${item.type !== "released" ? "pt-16" : "pt-8"}`}>
-        <span
-          className="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200"
-          aria-hidden="true"
-        ></span>
-        <div className="relative flex items-start space-x-3">
-          <div>
-            <div className="relative px-1">
-              <div
-                className={`h-8 w-8  rounded-full ring-8 ring-white dark:ring-slate-900 flex items-center justify-center ${
-                  item.type === "released" ? "bg-green-500" : "bg-sky-600"
-                }`}
-              >
-                {(item.type === "released" && <LuCheck className="icon-white" />) || (
-                  <BiQuestionMark className="icon-white" />
-                )}
+  const itemList = roadmapFile?.items.map((item: any, i: number) => {
+    const released = item.type === "released";
+    const key = item.date.replace(/[^a-zA-Z0-9]/g, "");
+
+    return (
+      <li key={key}>
+        <div className={`relative ${released ? "pt-8" : "pt-16"}`}>
+          <span
+            className="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200"
+            aria-hidden="true"
+          ></span>
+          <div className="relative flex items-start space-x-3">
+            <div>
+              <div className="relative px-1">
+                <div
+                  className={`h-8 w-8  rounded-full ring-8 ring-white dark:ring-slate-900 flex items-center justify-center ${
+                    released ? "bg-green-500" : "bg-sky-600"
+                  }`}
+                >
+                  {(released && <LuCheck className="icon-white" />) || (
+                    <BiQuestionMark className="icon-white" />
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="min-w-0 flex-1 py-0">
-            <div className="text-md text-gray-500">
-              <div>
-                {item.version && <button className="font-medium mr-2">{item.version}</button>}
+            <div className="min-w-0 flex-1 py-0">
+              <div className="text-md text-gray-500">
+                <div>
+                  {item.version && <button className="font-medium mr-2">{item.version}</button>}
 
-                <button className="my-0.5 relative inline-flex items-center bg-white rounded-full border border-gray-300 px-3 py-0.5 text-sm">
-                  <div className="absolute flex-shrink-0 flex items-center justify-center">
-                    <span
-                      className={`h-1.5 w-1.5 rounded-full ${
-                        item.type === "released" ? "bg-green-500" : "bg-orange-300"
-                      }`}
-                      aria-hidden="true"
-                    ></span>
-                  </div>
-                  <div className="ml-3.5 font-medium text-gray-900">
-                    <span>{roadmap[item.type as keyof typeof roadmap]}</span>
-                  </div>
-                </button>
-              </div>
-              <>
-                {(item.type === "Released" && (
+                  <button className="my-0.5 relative inline-flex items-center bg-white rounded-full border border-gray-300 px-3 py-0.5 text-sm">
+                    <div className="absolute flex-shrink-0 flex items-center justify-center">
+                      <span
+                        className={`h-1.5 w-1.5 rounded-full ${
+                          released ? "bg-green-500" : "bg-orange-300"
+                        }`}
+                        aria-hidden="true"
+                      ></span>
+                    </div>
+                    <div className="ml-3.5 font-medium text-gray-900">
+                      <span>{roadmap[item.type as keyof typeof roadmap]}</span>
+                    </div>
+                  </button>
+                </div>
+                {item.type === "Released" && (
                   <span className="whitespace-nowrap text-sm">
                     {new Date(item.date).toLocaleDateString(lang)}
                   </span>
-                )) || <></>}
-              </>
-            </div>
-            <h2 id={`h2-${i}`} className="text-lg font-semibold text-gray-900 dark:text-gray-100">
-              {item.heading[lang]}
-            </h2>
-            <div className="mt-2">
-              <ul className="list-disc list-inside">
-                {item.features[lang].map((feature: string, i: number) => (
-                  <li key={`listitem-${i}`}>
-                    {feature}
-                    <br />
-                  </li>
-                ))}
-              </ul>
+                )}
+              </div>
+              <h2
+                id={`h2-${i}`}
+                className="text-lg font-semibold text-gray-900 dark:text-gray-100"
+              >
+                {item.heading[lang]}
+              </h2>
+              <div className="mt-2">
+                <ul className="list-disc list-inside">
+                  {item.features[lang].map((feature: string, i: number) => (
+                    <li key={`${key}-${feature.replace(/[^a-zA-Z0-9]/g, "")}`}>
+                      {feature}
+                      <br />
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </li>
-  ));
+      </li>
+    );
+  });
 
   return (
     <div className="max-w-xl">
