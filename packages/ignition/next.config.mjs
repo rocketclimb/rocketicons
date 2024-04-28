137a2c36547a6b853fb0d43242c2b38d95d75e2f
Daniel Gomes <programad@gmail.com>
2024-04-28 09:19:49 -0300

    feat(next.js): optimizePackageImports

--- packages/ignition/next.config.mjs
+++ packages/ignition/next.config.mjs
@@ -8,26 +8,63 @@ import { createContentCollectionPlugin } from "@rocketclimb/content-collections"
 const shikiOptions = {
   themes: {
     light: "dracula",
-    dark: "dracula",
-  },
+    dark: "dracula"
+  }
 };
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     rehypePlugins: [
       [rehypeShiki, shikiOptions],
-      [rehypeSlug, { fragment: true, removeAccents: true }],
-    ],
-  },
+      [rehypeSlug, { fragment: true, removeAccents: true }]
+    ]
+  }
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+
+  experimental: {
+    optimizePackageImports: [
+      "rocketicons/ai",
+      "rocketicons/bi",
+      "rocketicons/bs",
+      "rocketicons/cg",
+      "rocketicons/ci",
+      "rocketicons/di",
+      "rocketicons/fa",
+      "rocketicons/fa6",
+      "rocketicons/fc",
+      "rocketicons/fi",
+      "rocketicons/gi",
+      "rocketicons/go",
+      "rocketicons/gr",
+      "rocketicons/hi",
+      "rocketicons/hi2",
+      "rocketicons/im",
+      "rocketicons/io",
+      "rocketicons/io5",
+      "rocketicons/lia",
+      "rocketicons/lu",
+      "rocketicons/md",
+      "rocketicons/pi",
+      "rocketicons/rc",
+      "rocketicons/ri",
+      "rocketicons/rx",
+      "rocketicons/si",
+      "rocketicons/sl",
+      "rocketicons/tb",
+      "rocketicons/tfi",
+      "rocketicons/ti",
+      "rocketicons/vsc",
+      "rocketicons/wi"
+    ]
+  }
 };
 
 const withContentCollections = createContentCollectionPlugin({
-  skipWatcher: true,
+  skipWatcher: true
 });
 
 export default withContentCollections(withMDX(nextConfig));
