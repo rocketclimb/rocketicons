df0046c3b27981b04283961470d92b4880221e19
Jeferson Amorim <amorimjj@gmail.com>
2024-04-28 16:24:10 -0300

    feat: fix missing escapes

--- packages/ignition/src/app/components/header.tsx
+++ packages/ignition/src/app/components/header.tsx
@@ -12,7 +12,7 @@ const LogoLink = ({ lang }: PropsWithLang) => {
   );
 };
 
-const VersionShower = () => (
+const VersionLabel = () => (
   <span className="transition-all duration-200 cursor-default ml-5 text-xs font-medium text-slate-500 dark:text-slate-400 bg-gray-200 dark:bg-slate-400/10 rounded-full py-1 px-3 items-center hover:bg-slate-400/20 dark:hover:bg-slate-400/40 ">
     {pkgVersion}
   </span>
@@ -22,7 +22,7 @@ const Header = ({ lang }: PropsWithLang) => (
   <div className="sticky top-0 z-40 w-full h-16 px-2 sm:px-8 py-4 backdrop-blur transition-colors duration-500 bg-white/95 lg:z-50  lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] dark:bg-slate-900/70">
     <div className="flex mx-auto items-center max-w-screen-2xl min-[1550px]:px-8">
       <LogoLink lang={lang} />
-      <VersionShower />
+      <VersionLabel />
       <MainNav lang={lang} />
     </div>
   </div>
