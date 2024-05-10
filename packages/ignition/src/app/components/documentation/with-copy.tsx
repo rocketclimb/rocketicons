b2dd0e30a1638e92e479e4f1a45ddacdcaa78741
Jeferson Amorim <amorimjj@gmail.com>
2024-05-10 09:15:04 -0300

    fix(no-changelog): allow use icon instead text

--- packages/ignition/src/app/components/documentation/with-copy.tsx
+++ packages/ignition/src/app/components/documentation/with-copy.tsx
@@ -1,25 +1,32 @@
 "use client";
 import useClipboard from "@/hooks/use-clipboard";
 import { withLocale } from "@/locales";
-import { PropsWithChildrenAndLang } from "@/types";
+import { Languages } from "@/types";
+import { PropsWithChildren } from "react";
 
 type WithCopyProps = {
   className?: string;
+  lang?: Languages;
   clipboardText: string;
-} & PropsWithChildrenAndLang;
+} & PropsWithChildren;
 
 const WithCopy = ({ lang, clipboardText, className, children }: WithCopyProps) => {
-  const { copy, copied } = withLocale(lang).config("code-block");
+  const useIcons = !lang;
+  const { copy, copied } = withLocale(lang || "en").config("code-block");
   const { write, isCurrent } = useClipboard(clipboardText);
+
+  const copyIcon = `\ue14f`;
+  const copiedIcon = `\ue179`;
 
   return (
     <button
+      data-icons={useIcons}
       data-copied={isCurrent()}
-      data-txt-copy={copy}
-      data-txt-copied={`${copied} ✓`}
+      data-txt-copy={useIcons ? copyIcon : copy}
+      data-txt-copied={useIcons ? copiedIcon : `${copied} ✓`}
       onClick={() => write()}
       onKeyDown={({ key }) => key === "c" && write()}
-      className={`relative after:hidden after:absolute after:w-30 after:border-slate-700 after:rounded after:py-0.5 after:px-1 hover:after:block hover:after:bg-slate-900 hover:after:border hover:after:content-[attr(data-txt-copy)] data-[copied=true]:after:content-[attr(data-txt-copied)] ${
+      className={`relative after:hidden after:absolute after:w-30 after:border-slate-700 after:rounded after:py-0.5 after:px-1 data-[icons=true]:after:text-2xl data-[icons=true]:after:mr-2 data-[icons=true]:after:font-icons hover:after:block hover:after:bg-slate-900 hover:after:border hover:after:content-[attr(data-txt-copy)] data-[copied=true]:after:content-[attr(data-txt-copied)] ${
         className ?? ""
       }`}
     >
