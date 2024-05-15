a8f7abec7de8ca7a4c418e61aadb1eb03e6b8078
Jeferson Amorim <amorimjj@gmail.com>
2024-05-14 22:20:27 -0300

    fix(no-changelog): table mobile and small screens

--- packages/ignition/src/app/components/table.tsx
+++ packages/ignition/src/app/components/table.tsx
@@ -87,12 +87,12 @@ export const Table = ({ lang, hasAdditional, collapse, children }: TableProps) =
             <tbody className="align-baseline">{children}</tbody>
           </table>
         </div>
-        <div className="hidden group-data-[collapse=true]/table:max-lg:block text-center backdrop-opacity-90 bg-white/30 dark:bg-slate-900/40 -mt-8 has-[:checked]:mt-0 has-[:checked]:sticky has-[:checked]:bottom-0 py-1.5">
-          <div className="text-sm py-1 dark:py-1.5 capitalize px-4 cursor-pointer dark:text-slate-50 dark:ring-1 ring-slate-100/20 dark:ring-inset font-semibold inline-block border rounded-xl bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600/10">
+        <div className="hidden group-data-[collapse=true]/table:max-lg:block text-center backdrop-opacity-90 bg-white/50 dark:bg-slate-900/75 -mt-8 has-[:checked]:mt-0 has-[:checked]:sticky has-[:checked]:bottom-0 py-1.5">
+          <div className="text-sm py-1 dark:py-1.5 px-4 cursor-pointer dark:text-slate-50 dark:ring-1 ring-slate-100/20 dark:ring-inset font-semibold inline-block border rounded-xl bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600/10">
             <input id="show-all" className="w-0 invisible" type="checkbox" name="show-all" />
             <label htmlFor="show-all" className="cursor-pointer">
-              <span className="showing-all">{showAll}</span>
-              <span className="showing-fewer">{showFewer}</span>
+              <span className="showing-all capitalize">{showAll}</span>
+              <span className="showing-fewer capitalize">{showFewer}</span>
             </label>
           </div>
         </div>
