5c0ad22b38b4797e0a7dd80fb57b821c678352a1
Jeferson Amorim <amorimjj@gmail.com>
2024-05-16 13:58:21 -0300

    fix(no-changelog): use content collections as cmd

--- packages/ignition/next.config.mjs
+++ packages/ignition/next.config.mjs
@@ -4,61 +4,8 @@ import rehypeShiki from "@shikijs/rehype";
 import rehypeSlug from "rehype-slug-custom-id";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-import { createContentCollectionPlugin } from "@rocketclimb/content-collections";
+import { shikiColorToClassTransform } from "@rocketclimb/code-block/shiki-tranform";
 import { IconsManifest } from "rocketicons/data";
-
-const shikiColorToClassTransform = () => {
-  let currentInfo = {
-    meta: "",
-    lang: "",
-    code: ""
-  };
-
-  return {
-    name: "rehype-custom-shiki:colors-to-class",
-    code(node) {
-      const colorToClass = (properties) => {
-        const { style } = properties;
-        const [, color] = /color:var\(--shiki-(.*)\)/.exec(style);
-        if (color) {
-          properties.class = `rc-cb-${color}`;
-          delete properties.style;
-        }
-      };
-      const replaceAllColors = (children) => {
-        children.forEach(({ children, properties }) => {
-          properties && properties.style && colorToClass(properties);
-          children && replaceAllColors(children);
-        });
-      };
-      replaceAllColors(node.children);
-      node.properties["data-clipboardText"] = Buffer.from(currentInfo.code).toString("base64");
-      return node;
-    },
-    pre(pre) {
-      const meta = currentInfo.meta
-        .split(" ")
-        .filter((meta) => !!meta)
-        .map((raw) => {
-          const [key, value] = raw.split("=");
-          return { key, value: value.replace(/"|'/g, "") };
-        });
-      meta?.forEach(({ key, value }) => {
-        pre.properties[`data-${key}`] = value;
-      });
-      pre.properties["data-lang"] = currentInfo.lang;
-      return pre;
-    },
-    preprocess(code, { meta: { __raw: meta }, lang }) {
-      currentInfo = {
-        meta,
-        lang,
-        code
-      };
-      return code;
-    }
-  };
-};
 
 const theme = createCssVariablesTheme({
   name: "css-variables",
@@ -97,8 +44,4 @@ const nextConfig = {
   }
 };
 
-const withContentCollections = createContentCollectionPlugin({
-  skipWatcher: true
-});
-
-export default withContentCollections(withMDX(nextConfig));
+export default withMDX(nextConfig);
