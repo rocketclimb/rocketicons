b1a9737de1ad75f971e8090a21775dab5e7af6cb
Jeferson Amorim <amorimjj@gmail.com>
2024-05-17 14:52:10 -0300

    fix(no-changelog): minor layout issues

--- packages/ignition/src/app/components/sidebar/playground.tsx
+++ packages/ignition/src/app/components/sidebar/playground.tsx
@@ -12,7 +12,7 @@ const Playground = () =>
         href={playgroundUrl}
         target="_blank"
       >
-        <div className="mr-4 size-6 text-center rounded-md ring-1 ring-slate-900/5 shadow-sm group-hover/highligh-link:shadow group-hover/highligh-link:ring-slate-900/10 dark:ring-0 dark:shadow-none dark:group-hover/highligh-link:shadow-none dark:group-hover/highligh-link:highlight-white/10 group-hover/highligh-link:shadow-blue-200 dark:group-hover/highligh-link:bg-blue-500 dark:bg-slate-800 dark:highlight-white/5">
+        <div className="mr-4 size-6 text-center leading-[22px] rounded-md ring-1 ring-slate-900/5 shadow-sm group-hover/highligh-link:shadow group-hover/highligh-link:ring-slate-900/10 dark:ring-0 dark:shadow-none dark:group-hover/highligh-link:shadow-none dark:group-hover/highligh-link:highlight-white/10 group-hover/highligh-link:shadow-blue-200 dark:group-hover/highligh-link:bg-blue-500 dark:bg-slate-700 lg:dark:bg-slate-800 dark:highlight-white/5">
           <MdCode className="icon-sky-500-sm opacity-75 group-hover/highligh-link:opacity-100 group-hover/highligh-link:dark:icon-slate-300-sm" />
         </div>
         Playground
