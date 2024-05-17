4be8d3839ca13e3c96b6a4405ca2043449d852b5
Jeferson Amorim <amorimjj@gmail.com>
2024-05-17 09:43:56 -0300

    fix(no-changelog): move playground into sidebar

--- packages/ignition/src/config/index.ts
+++ packages/ignition/src/config/index.ts
@@ -1,9 +1,1 @@
-import { getEdgeConfig } from "./edge";
-
 export * from "./site";
-
-export const getConfig = getEdgeConfig;
-
-export const config = {
-  getPlaygroundUrl: async () => getConfig("playgroundUrl")
-};
