85ede5306b6a675d49d605e323fe8586b7d18f39
Jeferson Amorim <amorimjj@gmail.com>
2024-06-06 23:22:57 -0300

    fix(no-changelog): decrease function size

--- packages/ignition/src/app/utils/svg-as-json.ts
+++ packages/ignition/src/app/utils/svg-as-json.ts
@@ -1,12 +1,21 @@
 import AdmZip from "adm-zip";
 import { join, resolve } from "node:path";
 import { CollectionID } from "rocketicons/data";
+import sqlite3 from "sqlite3";
 
 const DATA_DIR = "./src/app/data-helpers/svgs/";
 const DATA_FILE = join(DATA_DIR, "jsons.zip");
+const DB_FILE = resolve(DATA_DIR, "svg.db");
 
 export const svgAsJson = (collectionId: CollectionID, iconId: string) => {
-  const zip = new AdmZip(resolve(DATA_FILE));
   const filename = join(collectionId, `${iconId}.json`);
+  const db = new sqlite3.Database(DB_FILE, sqlite3.OPEN_READWRITE);
+  db.serialize(() => {
+    db.each(`SELECT svg FROM svgs WHERE id = "${filename}"`, (err, row) => console.log(row));
+  });
+  db.close();
+
+  const zip = new AdmZip(resolve(DATA_FILE));
+
   return JSON.parse(zip.readAsText(filename));
 };
--- packages/ignition/src/statics-generator/svg-files.ts
+++ packages/ignition/src/statics-generator/svg-files.ts
@@ -1,15 +1,26 @@
-import { existsSync, readFileSync, copyFileSync, readdirSync, rmSync } from "node:fs";
-import { join } from "node:path";
+import { existsSync, readFileSync, copyFileSync, readdirSync, rmSync, mkdirSync } from "node:fs";
+import { join, resolve } from "node:path";
+import sqlite3, { Statement } from "sqlite3";
 import AdmZip from "adm-zip";
 
 import { getContents } from "./utils";
 
 const SOURCE_SVGS = "../generator/svgs";
 const DEST_SVGS = "./src/app/data-helpers/svgs/";
+const DEST_SQL_SVGS = resolve("./src/app/data-helpers/svgs/svg.db");
 
 const hasChanges = () =>
   readFileSync(`${SOURCE_SVGS}/.lock`).toString() !==
   readFileSync(`${DEST_SVGS}/.lock`).toString();
+
+const handleFilesS = (files: string[], collection: string, stmt: Statement) => {
+  files
+    .filter((filename) => filename.endsWith(".json"))
+    .forEach((filename) => {
+      const contents = getContents(join(SOURCE_SVGS, collection, filename), true);
+      stmt.run([join(collection, filename), contents]);
+    });
+};
 
 const handleFiles = (files: string[], collection: string, zip: AdmZip) => {
   files
@@ -25,6 +36,24 @@ const generator = async () => {
     rmSync(DEST_SVGS, { recursive: true, force: true });
     const zip = new AdmZip();
     const collections = readdirSync(SOURCE_SVGS, { withFileTypes: true });
+    mkdirSync(DEST_SVGS, { recursive: true });
+    const db = new sqlite3.Database(DEST_SQL_SVGS, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE);
+
+    db.serialize(() => {
+      db.run("CREATE TABLE svgs (id STRING PRIMARY KEY, svg TEXT)");
+
+      const stmt = db.prepare("INSERT INTO svgs VALUES (?, ?)");
+      collections
+        .filter((entry) => entry.isDirectory())
+        .forEach(({ name }) => {
+          const files = readdirSync(join(SOURCE_SVGS, name));
+          handleFilesS(files, name, stmt);
+        });
+      stmt.finalize();
+    });
+
+    db.close();
+
     collections
       .filter((entry) => entry.isDirectory())
       .forEach(({ name }) => {
