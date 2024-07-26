c383d05b0777eac03de994cb8a17d10057dac81c
Daniel Gomes <programad@gmail.com>
2024-07-26 12:37:36 -0300

    fix(icons): correct handling of tailwind prefix

--- packages/tailwind/src/index.ts
+++ packages/tailwind/src/index.ts
@@ -4,15 +4,47 @@ import { CSSRuleObject, PluginAPI } from "tailwindcss/types/config";
 import DefaultTheme from "./theme";
 import { stylesGenerator } from "./utils/styles-generator";
 import { configHandler } from "./utils/config-handler";
+import { ThemeOptions } from "./types";
+
+const addPrefixToTheme = (theme: ThemeOptions, prefix: string): ThemeOptions => {
+  const baseStyleClasses = theme.baseStyle?.split(" ") ?? [];
+
+  const baseStyle = baseStyleClasses.reduce((acc: string, value) => {
+    return (acc += `${prefix}${value} `);
+  }, "");
+
+  const sizes = Object.entries(theme.sizes).reduce(
+    (acc: { [key: string]: string }, [key, value]) => {
+      const sizeClasses = value.split(" ") ?? [];
+
+      const newClassesArray = sizeClasses.reduce((acc: string[], value) => {
+        return [...acc, `${prefix}${value}`];
+      }, []);
+
+      acc[key] = newClassesArray.join(" ");
+      return acc;
+    },
+    {}
+  );
+
+  return {
+    ...theme,
+    baseStyle,
+    sizes
+  };
+};
 
 const pluginFactory = (isNative: boolean = false) => {
   return plugin(
     ({ addComponents, config }: PluginAPI) => {
-      const prefix = config("prefix") ?? "";
-      const classPrefix = `${prefix}icon`;
-      const handler = configHandler(config);
-      const theme = handler("icon", DefaultTheme);
-      const generator = stylesGenerator(classPrefix, isNative);
+      const prefix = (config("prefix") as string) ?? "";
+      const themeModified = addPrefixToTheme(DefaultTheme, prefix);
+
+      const handler = configHandler(config, prefix);
+      const theme = handler("icon", themeModified);
+
+      const generator = stylesGenerator("icon", isNative);
+
       const styles = generator
         .add(theme.defaults())
         .add(theme.sizes())
