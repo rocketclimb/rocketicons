61d5be8fac28424afed525045d46735801373dd1
Jeferson Amorim <amorimjj@gmail.com>
2025-09-03 10:35:56 -0300

    fix(core): fix lint issues

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -8,22 +8,19 @@ import {
   CollectionDataInfo
 } from "@/types";
 
-export const IconGenerator =
-  (
-    data: IconTree,
-    variant: Variants,
-    name: string
-  ): IconType => // eslint-disable-next-line react/display-name
-  ({ className, ...props }: IconBaseProps) => (
-    <svg
-      {...data.attr}
-      {...props}
-      className={handleClassName(variant, className ?? "")}
-      data-icon-name={name}
-    >
-      {tree2Element(data.child)}
-    </svg>
-  );
+export const IconGenerator = (data: IconTree, variant: Variants, name: string): IconType =>
+  function Icon({ className, ...props }: IconBaseProps) {
+    return (
+      <svg
+        {...data.attr}
+        {...props}
+        className={handleClassName(variant, className ?? "")}
+        data-icon-name={name}
+      >
+        {tree2Element(data.child)}
+      </svg>
+    );
+  };
 
 export {
   IconTree,
